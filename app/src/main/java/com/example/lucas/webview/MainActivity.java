c1809091fdf8300fa9e32cd28c904d5d6636837d
Augusto <gstfrr@gmail.com>
2017-06-27 11:54:47 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lucas/webview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.lucas.webview;
+
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        }, 0);
+
+        //url do servidor
+        String url = "http://177.105.60.225:7000/visual.html";
+
+        WebView webview = new WebView(this);
+        webview.setWebViewClient(new WebViewClient());
+        setContentView(webview);
+        //webview.clearCache(true);
+        //webview.clearHistory();
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+
+        webview.loadUrl(url);
+    }
+}
